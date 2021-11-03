58fcd97f6de92cf55a4e5e33cc7472ec120b60a9
hey-chan <liumatthew8@gmail.com>
2021-11-03 21:18:59 +1100

    Added intro page and asset pics

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -3,8 +3,24 @@ import './intro.scss'
 export default function Intro() {
   return (
     <div className="intro" id="intro">
-      <p>Intro. Welcome to Matt's portfolio</p>
-    </div>
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/me.png" alt="matt-liu" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hey there, I'm</h2>
+          <h1>Matt</h1>
+          <h3>Aspiring software developer</h3>
+          {/* <h3><span></span></h3> */}
+        </div>
+        <a href="#aboutme">
+          <img src="assets/arrow.png" alt=""/>
+        </a>
+      </div>
+      {/* <p>Intro. Welcome to Matt's portfolio</p> */}
+    </div> 
   )
 }
  
