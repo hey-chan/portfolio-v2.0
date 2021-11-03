0616552e0d919278727ea8699d9fe0094affd559
hey-chan <liumatthew8@gmail.com>
2021-11-03 21:33:40 +1100

    animation for text typing

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,6 +1,19 @@
-import './intro.scss'
+import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Software developer", "Designer"],
+    });
+  }, []);
+
   return (
     <div className="intro" id="intro">
       <div className="left">
@@ -12,15 +25,16 @@ export default function Intro() {
         <div className="wrapper">
           <h2>Hey there, I'm</h2>
           <h1>Matt</h1>
-          <h3>Aspiring software developer</h3>
-          {/* <h3><span></span></h3> */}
+          {/* <h3>Aspiring software developer</h3> */}
+          <h3>
+            Aspiring <span ref={textRef}></span>
+          </h3>
         </div>
         <a href="#aboutme">
-          <img src="assets/arrow.png" alt=""/>
+          <img src="assets/arrow.png" alt="" />
         </a>
       </div>
       {/* <p>Intro. Welcome to Matt's portfolio</p> */}
-    </div> 
-  )
+    </div>
+  );
 }
- 
