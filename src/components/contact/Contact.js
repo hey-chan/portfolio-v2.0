c5d9d6018ae2bcbe6bcb2613830d9a5de80b59e7
hey-chan <liumatthew8@gmail.com>
2021-11-07 14:44:24 +1100

    implemented emailjs for sending message

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,12 +1,25 @@
 import './contact.scss'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
+import emailjs from 'emailjs-com';
+
 
 export default function Contact() {
 
-  const [message, setMessage] = useState(false)
+  // const [message, setMessage] = useState(false)
+  const [done, setDone] = useState(false)
+
+  const formRef = useRef()
+
   const handleSubmit = (e) => {
     e.preventDefault()
-    setMessage(true)
+
+    emailjs.sendForm('service_ddah1ys', 'template_10ebyx9', formRef.current, 'user_Mujggk877bkjDoeWHkVM7')
+    .then((result) => {
+        console.log(result.text);
+        setDone(true)
+    }, (error) => {
+        console.log(error.text);
+    });
   }
 
   return (
@@ -16,11 +29,13 @@ export default function Contact() {
       </div>
       <div className="right">
         <h2>Contact</h2>
-        <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="Email" />
-          <textarea placeholder="Message"></textarea>
+        <form ref={formRef} onSubmit={handleSubmit}>
+          <input type="text" placeholder="Name" name="user_name"/>
+          <input type="text" placeholder="Subject" name="user_subject" />
+          <input type="text" placeholder="Email" name="user_email"/>
+          <textarea placeholder="Message" name="message"></textarea>
           <button type="submit">Send</button>
-          {message && <span>Thanks, I will get back to you</span>}
+          {done && <span>Thanks, I will get back to you</span>}
         </form>
       </div>
     </div>
